bd76e648b04e698f360d0ed9fb397c342573cf12
KZeTa <kztch19@gmail.com>
2026-07-22 16:43:59 -0500

    Borde a panelGrafo

--- Paneles/PanelGrafo.java
+++ Paneles/PanelGrafo.java
@@ -19,6 +19,7 @@ public class PanelGrafo extends JPanel {
     private boolean modoConectar;
 
     public PanelGrafo( Grafo grafo) {
+        setBorder(BorderFactory.createTitledBorder("Visualización"));
         this.grafo=grafo;
         inicializarMenuContextual();
 
